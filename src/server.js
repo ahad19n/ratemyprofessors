903791b4fe094afb57425dc310a39d8dccf1211e
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2025-09-08 15:15:18 +0500

    Initial Commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const layouts = require('express-ejs-layouts');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(layouts);
+app.set('view engine', 'ejs');
+app.set("layout", "layouts/main");
+app.set('views', path.join(__dirname, 'views'));
+app.use('/assets', express.static(path.join(__dirname, '../assets')));
+
+app.use(require('./router'));
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
