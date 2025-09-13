e7b28f6bfa246f12063dcb50d4f604a9e3349d1b
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2025-09-13 14:23:23 +0500

    temp change

--- src/models/Professor.js
+++ src/models/Professor.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
   name: { type: String, required: true },
   slug: { type: String, required: true },
-  university: { required: true, ref: University, type: mongoose.Schema.Types.ObjectId }
+  // university: { required: true, ref: University, type: mongoose.Schema.Types.ObjectId }
 });
 
 module.exports = new mongoose.model("Professor", schema);
