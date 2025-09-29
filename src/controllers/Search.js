1f4188b069bd4c67a443a7527316850d5ff3d8fe
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2025-09-30 02:44:34 +0500

    nigga chain x2

--- src/controllers/Search.js
+++ src/controllers/Search.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send("Search Controller")
+const Professor = require('../models/Professor');
+
+router.get('/', async (req, res) => {
+  const rawQuery = req.query?.q;
+  const queryParts = rawQuery.trim().split(/\s+/);
+
+  const results = await Professor.find({
+    $or: queryParts.map(part => ({
+      fullName: { $regex: part, $options: "i" }
+    }))
+  }).populate('university');
+
+  res.render('SearchResults', { rawQuery, results });
 });
 
 module.exports = router;
