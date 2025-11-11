eb3ed684e0058dd887e10cb9ce3a06967bda62d6
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2025-11-11 17:39:28 +0500

    add handler to serve image

--- src/controllers/Async.js
+++ src/controllers/Async.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.send("Async Controller");
+const Professor = require("./Professor");
+const ProfessorImage = require("./ProfessorImage");
+
+router.get("/getImage/:slug", async (req, res) => {
+  try {
+    const { slug } = req.params;
+
+    // Find the professor by slug
+    const professor = await Professor.findOne({ slug });
+    if (!professor) {
+      return res.status(404).send("Professor not found");
+    }
+
+    // Find the image for that professor
+    const imageDoc = await ProfessorImage.findOne({ professor: professor._id });
+    if (!imageDoc) {
+      return res.status(404).send("Image not found");
+    }
+
+    // Set the correct content type and send the image buffer
+    res.set("Content-Type", imageDoc.contentType);
+    res.send(imageDoc.image);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 });
 
 module.exports = router;
