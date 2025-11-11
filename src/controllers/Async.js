871f5bf20f0eb731506469a02cc883f912fef62b
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2025-11-11 19:28:26 +0500

    add cache control

--- src/controllers/Async.js
+++ src/controllers/Async.js
@@ -8,20 +8,19 @@ router.get("/getImage/:slug", async (req, res) => {
   try {
     const { slug } = req.params;
 
-    // Find the professor by slug
     const professor = await Professor.findOne({ slug });
-    if (!professor) {
-      return res.status(404).send("Professor not found");
-    }
+    if (!professor) return res.status(404).send("Professor not found");
 
-    // Find the image for that professor
     const imageDoc = await ProfessorImage.findOne({ professor: professor._id });
-    if (!imageDoc) {
-      return res.status(404).send("Image not found");
-    }
+    if (!imageDoc) return res.status(404).send("Image not found");
 
-    // Set the correct content type and send the image buffer
-    res.set("Content-Type", imageDoc.contentType);
+    // Cache for 1 hour
+    res.set({
+      "Content-Type": imageDoc.contentType,
+      "Cache-Control": "public, max-age=3600, s-maxage=3600", // 1 hour
+      "Expires": new Date(Date.now() + 3600 * 1000).toUTCString(), // 1 hour
+    });
+
     res.send(imageDoc.image);
   } catch (err) {
     console.error(err);
